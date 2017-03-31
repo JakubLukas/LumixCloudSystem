147935190233003ff5fb0159bada656c304cfa8c
JakubLukas <mucha.godlike@gmail.com>
2017-03-31 09:41:16 +0200

    Repo cleanup

--- data/pipelines/compiled/clouds_CLOUDS0_vs.shb.d
+++
@@ -1,6 +0,0 @@
-d:/projects/lumixcloudsystem/data//pipelines/compiled/clouds_CLOUDS0_vs.shb :  \
- d:/projects/lumixcloudsystem/data//pipelines/varying.def.sc \
- d:/projects/lumixcloudsystem/data//pipelines/common.sh \
- d:/projects/lumixcloudsystem/data//pipelines/common/bgfx_shader.sh \
- d:/projects/lumixcloudsystem/data//pipelines/common/shaderlib.sh \
- d:/projects/lumixcloudsystem/data//pipelines/common/lumix_common.sh
--- data/pipelines/compiled/rigid_DEFERRED304_fs.shb.d
+++
@@ -1,6 +0,0 @@
-d:/projects/lumixengine_data//pipelines/compiled/rigid_DEFERRED304_fs.shb :  \
- d:/projects/lumixengine_data//pipelines/varying.def.sc \
- d:/projects/lumixengine_data//pipelines/common.sh \
- d:/projects/lumixengine_data//pipelines/common/bgfx_shader.sh \
- d:/projects/lumixengine_data//pipelines/common/shaderlib.sh \
- d:/projects/lumixengine_data//pipelines/common/lumix_common.sh
